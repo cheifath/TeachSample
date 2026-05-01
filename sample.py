61456ed0f47b91febccee6d87891cc589df33a71
Cheifath <cheifathmohammed@gmail.com>
2026-05-01 20:37:04 +0530

    sample

---
+++ sample.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+def login(username, password):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
+    cursor.execute(query)
+
+    user = cursor.fetchone()
+
+    if user:
+        return "Login successful"
+    else:
+        return "Invalid credentials"
